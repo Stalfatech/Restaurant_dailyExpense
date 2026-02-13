1faa60f745597f1cac934038b93dd7c2ac75118c
Sneha Suresh <snehasuresh98723@gmail.com>
2026-02-13 10:47:06 +0530

    main

---
+++ .history/Restaurant_dailysale/App/models_20260213104037.py
@@ -0,0 +1,84 @@
+import uuid
+from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.db.models import Sum
+from django.conf import settings
+from datetime import date
+
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("Email is required")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+        return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractUser):
+    USER_TYPE_CHOICES = (
+        (0, 'Admin'),
+        (1, 'Manager'),
+        (2, 'Staff'),
+    )
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    name = models.CharField(max_length=100, null=True, blank=True)
+
+    username = None
+    email = models.EmailField(unique=True, max_length=191)
+
+    user_type = models.IntegerField(null=True,choices=USER_TYPE_CHOICES)
+    branch = models.ForeignKey("Branch", on_delete=models.SET_NULL, null=True)
+
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='custom_user_set',
+        blank=True
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='custom_user_permissions_set',
+        blank=True
+    )
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+
+class Register(models.Model):
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    name = models.CharField(max_length=191, null=True)
+    contact = models.BigIntegerField(null=True)
+    loginid = models.ForeignKey(User, on_delete=models.CASCADE, related_name="registrations")
+
+class Manager(models.Model):
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name="manager_profile"
+    )
+
+    phone = models.BigIntegerField()
+    dob = models.DateField()
+    gender = models.CharField(max_length=10)
+    address = models.TextField()
+    joining_date = models.DateField()
+    photo = models.ImageField(upload_to='managers/')
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.name
---
+++ .history/Restaurant_dailysale/App/urls_20260213104527.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.register, name='register'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('topbar', views.topbar_view, name='topbar'),
+    path('login', views.Logindata, name='login'),
+    path('logouts',views.Logouts),
+    path('reset_password/<int:user_id>/', views.reset_password, name='reset_password'),
+    path('forgot_password/', views.forgot_password, name='forgot_password'),
+    path('add_manager/', views.add_manager, name='manager_add'),
+    path('manager_view', views.manager_view, name='manager_view'),
+    path('manager_edit/<int:id>/', views.manager_edit, name='manager_edit'),
+    path('manager_delete/<int:id>/', views.manager_delete, name='manager_delete'),
+
+    
+
+]

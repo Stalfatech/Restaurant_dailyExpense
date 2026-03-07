c4b2822d3fb0b905636f68c764a755e3112bf7bd
Divyacharu <divyasreehtc003@gmail.com>
2026-03-07 11:43:13 +0530

    dashboard_update

--- Restaurant_dailysale/App/urls.py
+++ Restaurant_dailysale/App/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.register, name='register'),
@@ -7,8 +8,9 @@ urlpatterns = [
     path('topbar', views.topbar_view, name='topbar'),
     path('login', views.Logindata, name='login'),
     path('logouts',views.Logouts, name='logouts'),
-    path('reset_password/<int:user_id>/', views.reset_password, name='reset_password'),
-    path('forgot_password/', views.forgot_password, name='forgot_password'),
+    #  path('reset_password/<int:user_id>/', views.reset_password, name='reset_password'),
+    path('reset_password/<uidb64>/<token>/', views.reset_password, name='reset_password'),
+     path('forgot_password/', views.forgot_password, name='forgot_password'),
     path('add_manager/', views.add_manager, name='manager_add'),
     path('manager_view', views.manager_view, name='manager_view'),
     path('manager_edit/<int:id>/', views.manager_edit, name='manager_edit'),
